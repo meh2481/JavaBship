290eab5fffe7eef033a7757c89766ad2a65a20d8
RetiredSphinx <meh2481@vt.edu>
2016-01-13 16:12:56 -0500

    Initial Version

---
+++ core/src/com/meh2481/battleship/MyBattleshipGame.java
@@ -0,0 +1,139 @@
+package com.meh2481.battleship;
+
+import java.util.Iterator;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.TimeUtils;
+
+public class MyBattleshipGame extends ApplicationAdapter
+{
+	private Texture shipCenterImage;
+	private Texture shipEdgeImage;
+	private Sound missSound;
+	private Music beginMusic;
+	private SpriteBatch batch;
+	private OrthographicCamera camera;
+	private Rectangle bucket;
+	private Array<Rectangle> raindrops;
+	private long lastDropTime;
+
+	@Override
+	public void create()
+	{
+		// load the images for the droplet and the bucket, 64x64 pixels each
+		shipCenterImage = new Texture(Gdx.files.internal("ship_center.png"));
+		shipEdgeImage = new Texture(Gdx.files.internal("ship_edge.png"));
+
+		// load the drop sound effect and the rain background "music"
+		missSound = Gdx.audio.newSound(Gdx.files.internal("miss.ogg"));
+		beginMusic = Gdx.audio.newMusic(Gdx.files.internal("beginningMusic.ogg"));
+
+		// start the playback of the background music immediately
+		beginMusic.setLooping(true);
+		beginMusic.play();
+
+		// create the camera and the SpriteBatch
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 600);
+		batch = new SpriteBatch();
+
+		// create a Rectangle to logically represent the bucket
+		bucket = new Rectangle();
+		bucket.x = 800 / 2 - 64 / 2; // center the bucket horizontally
+		bucket.y = 20; // bottom left corner of the bucket is 20 pixels above the bottom screen edge
+		bucket.width = 64;
+		bucket.height = 64;
+
+		// create the raindrops array and spawn the first raindrop
+		raindrops = new Array<Rectangle>();
+		spawnRaindrop();
+	}
+
+	private void spawnRaindrop()
+	{
+		Rectangle raindrop = new Rectangle();
+		raindrop.x = MathUtils.random(0, 800-64);
+		raindrop.y = 600;
+		raindrop.width = 64;
+		raindrop.height = 64;
+		raindrops.add(raindrop);
+		lastDropTime = TimeUtils.nanoTime();
+	}
+
+	@Override
+	public void render()
+	{
+		// clear the screen with a dark blue color. The
+		// arguments to glClearColor are the red, green
+		// blue and alpha component in the range [0,1]
+		// of the color to be used to clear the screen.
+		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		// tell the camera to update its matrices.
+		camera.update();
+
+		// tell the SpriteBatch to render in the
+		// coordinate system specified by the camera.
+		batch.setProjectionMatrix(camera.combined);
+
+		// begin a new batch and draw the bucket and
+		// all drops
+		batch.begin();
+		batch.draw(shipEdgeImage, bucket.x, bucket.y);
+		for(Rectangle raindrop: raindrops)
+		{
+			batch.draw(shipCenterImage, raindrop.x, raindrop.y);
+		}
+		batch.end();
+
+		// process user input
+		if(Gdx.input.isKeyPressed(Keys.LEFT)) bucket.x -= 600 * Gdx.graphics.getDeltaTime();
+		if(Gdx.input.isKeyPressed(Keys.RIGHT)) bucket.x += 600 * Gdx.graphics.getDeltaTime();
+
+		// make sure the bucket stays within the screen bounds
+		if(bucket.x < 0) bucket.x = 0;
+		if(bucket.x > 800 - 64) bucket.x = 800 - 64;
+
+		// check if we need to create a new raindrop
+		if(TimeUtils.nanoTime() - lastDropTime > 1000000000) spawnRaindrop();
+
+		// move the raindrops, remove any that are beneath the bottom edge of
+		// the screen or that hit the bucket. In the later case we play back
+		// a sound effect as well.
+		Iterator<Rectangle> iter = raindrops.iterator();
+		while(iter.hasNext())
+		{
+			Rectangle raindrop = iter.next();
+			raindrop.y -= 200 * Gdx.graphics.getDeltaTime();
+			if(raindrop.y + 64 < 0) iter.remove();
+			if(raindrop.overlaps(bucket))
+			{
+				missSound.play();
+				iter.remove();
+			}
+		}
+	}
+
+	@Override
+	public void dispose()
+	{
+		// dispose of all the native resources
+		shipCenterImage.dispose();
+		shipEdgeImage.dispose();
+		missSound.dispose();
+		beginMusic.dispose();
+		batch.dispose();
+	}
+}
---
+++ desktop/src/com/meh2481/battleship/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.meh2481.battleship.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.meh2481.battleship.MyBattleshipGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Battleship";
+		config.width = 800;
+		config.height = 600;
+		new LwjglApplication(new MyBattleshipGame(), config);
+	}
+}
